2ed158289aa4408548fa49115c11bfd01cb025eb
diajain315 <jaindiya.k@gmail.com>
2025-10-19 22:24:41 +0530

    updated snake inside box

--- snake_gamebox.cpp
+++ snake_gamebox.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-using namespace std;
+using namespace std;    
+int main() {
+    const int BOX_HEIGHT = 12;
+    const int BOX_WIDTH = 30;
 
-int main() 
-{
-    
-    const int HEIGHT = 12; 
-    const int WIDTH = 30; 
+    int snakePosX = 5;
+    int snakePosY = 5;
+    int snakeLength = 6;
 
-    
-    for (int row = 0; row < HEIGHT; ++row) 
-    {
-        
-        for (int col = 0; col < WIDTH; ++col) 
-        {
-           
-            if (row == 0 || row == HEIGHT - 1 || col == 0 || col == WIDTH - 1) 
-            {
-                cout << '#';  
-            } else 
-            {
-                cout << ' ';  
-            }
+    for (int row = 0; row < BOX_HEIGHT; ++row) {
+        for (int col = 0; col < BOX_WIDTH; ++col) {
+            if (row == 0 || row == BOX_HEIGHT - 1 || col == 0 || col == BOX_WIDTH - 1)
+                cout << '#';
+            else if (row == snakePosY && col >= snakePosX && col < snakePosX + snakeLength)
+                cout << 'O';
+            else
+                cout << ' ';
         }
-        cout << '\n'; 
+        cout << '\n';
     }
 
     return 0;
 }
+
+
+
+
+
+
