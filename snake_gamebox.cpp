847b746377f9301d0d6f57d8dadbd97e06e3b113
diajain315 <jaindiya.k@gmail.com>
2025-10-21 21:38:21 +0530

    Update snake_gamebox.cpp
    Moving The snake manually by the keys W A S D

--- snake_gamebox.cpp
+++ snake_gamebox.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 int main() {
@@ -12,18 +14,39 @@ int main() {
     int foodX = 30;
     int foodY = 12;
 
-    for (int row = 0; row < BOX_HEIGHT; ++row) {
-        for (int col = 0; col < BOX_WIDTH; ++col) {
-            if (row == 0 || row == BOX_HEIGHT - 1 || col == 0 || col == BOX_WIDTH - 1)
-                cout << '#';
-            else if (row == snakePosY && col >= snakePosX && col < snakePosX + snakeLength)
-                cout << 'O';
-            else if (row == foodY && col == foodX)
-                cout << 'F';
-            else
-                cout << ' ';
+    char dir = 'd';
+
+    while (true) {
+        system("cls");
+
+        for (int row = 0; row < BOX_HEIGHT; ++row) {
+            for (int col = 0; col < BOX_WIDTH; ++col) {
+                if (row == 0 || row == BOX_HEIGHT - 1 || col == 0 || col == BOX_WIDTH - 1)
+                    cout << '#';
+                else if (row == snakePosY && col >= snakePosX && col < snakePosX + snakeLength)
+                    cout << 'O';
+                else if (row == foodY && col == foodX)
+                    cout << 'F';
+                else
+                    cout << ' ';
+            }
+            cout << '\n';
         }
-        cout << '\n';
+
+        if (cin.peek() != '\n') cin >> dir;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        if (dir == 'w') snakePosY--;
+        else if (dir == 's') snakePosY++;
+        else if (dir == 'a') snakePosX--;
+        else if (dir == 'd') snakePosX++;
+
+        if (snakePosX <= 0) snakePosX = 1;
+        if (snakePosX + snakeLength >= BOX_WIDTH - 1) snakePosX = BOX_WIDTH - snakeLength - 1;
+        if (snakePosY <= 0) snakePosY = 1;
+        if (snakePosY >= BOX_HEIGHT - 1) snakePosY = BOX_HEIGHT - 2;
+
+        this_thread::sleep_for(chrono::milliseconds(200));
     }
 
     return 0;
