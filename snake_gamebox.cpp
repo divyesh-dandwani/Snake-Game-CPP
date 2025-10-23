e3138fa18c1efc11c6faa2ed848158095900bc41
diajain315 <jaindiya.k@gmail.com>
2025-10-24 00:19:39 +0530

    Update snake_gamebox.cpp
    Making snake movement realistic like vertical and horizontal

--- snake_gamebox.cpp
+++ snake_gamebox.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
 
 int main() {
     const int BOX_HEIGHT = 20;
     const int BOX_WIDTH = 50;
 
-    int snakePosX = 10;
-    int snakePosY = 10;
+    vector<Point> snake;
+    snake.push_back({10, 10});
     int snakeLength = 3;
     int score = 0;
 
     srand(time(0));
-    int foodX = rand() % (BOX_WIDTH - 2) + 1;
-    int foodY = rand() % (BOX_HEIGHT - 2) + 1;
+    Point food = {rand() % (BOX_WIDTH - 2) + 1, rand() % (BOX_HEIGHT - 2) + 1};
 
     char dir = 'd';
     bool gameOver = false;
@@ -29,12 +34,21 @@ int main() {
             for (int col = 0; col < BOX_WIDTH; ++col) {
                 if (row == 0 || row == BOX_HEIGHT - 1 || col == 0 || col == BOX_WIDTH - 1)
                     cout << '#';
-                else if (row == snakePosY && col >= snakePosX && col < snakePosX + snakeLength)
-                    cout << 'O';
-                else if (row == foodY && col == foodX)
-                    cout << 'F';
-                else
-                    cout << ' ';
+                else {
+                    bool printed = false;
+                    if (row == food.y && col == food.x) {
+                        cout << 'F';
+                        printed = true;
+                    }
+                    for (auto s : snake) {
+                        if (row == s.y && col == s.x) {
+                            cout << 'O';
+                            printed = true;
+                            break;
+                        }
+                    }
+                    if (!printed) cout << ' ';
+                }
             }
             cout << '\n';
         }
@@ -42,21 +56,30 @@ int main() {
         if (cin.peek() != '\n') cin >> dir;
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        if (dir == 'w') snakePosY--;
-        else if (dir == 's') snakePosY++;
-        else if (dir == 'a') snakePosX--;
-        else if (dir == 'd') snakePosX++;
+        Point newHead = snake[0];
+        if (dir == 'w') newHead.y--;
+        else if (dir == 's') newHead.y++;
+        else if (dir == 'a') newHead.x--;
+        else if (dir == 'd') newHead.x++;
 
-        if (snakePosY == foodY && snakePosX <= foodX && foodX < snakePosX + snakeLength) {
-            snakeLength++;
-            score++;
-            foodX = rand() % (BOX_WIDTH - 2) + 1;
-            foodY = rand() % (BOX_HEIGHT - 2) + 1;
-        }
-
-        if (snakePosX <= 0 || snakePosX + snakeLength >= BOX_WIDTH - 1 || snakePosY <= 0 || snakePosY >= BOX_HEIGHT - 1) {
+        if (newHead.x <= 0 || newHead.x >= BOX_WIDTH - 1 || newHead.y <= 0 || newHead.y >= BOX_HEIGHT - 1) {
             gameOver = true;
         }
+        for (auto s : snake) {
+            if (newHead.x == s.x && newHead.y == s.y) {
+                gameOver = true;
+            }
+        }
+
+        snake.insert(snake.begin(), newHead);
+        if (newHead.x == food.x && newHead.y == food.y) {
+            score++;
+            food.x = rand() % (BOX_WIDTH - 2) + 1;
+            food.y = rand() % (BOX_HEIGHT - 2) + 1;
+        } else {
+            while (snake.size() > snakeLength) snake.pop_back();
+        }
+        snakeLength = snake.size();
 
         this_thread::sleep_for(chrono::milliseconds(200));
     }
@@ -67,5 +90,3 @@ int main() {
 
     return 0;
 }
-
-
