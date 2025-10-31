55774bfdaf4922ace4e76c4b34b83fe6680a4f01
diajain315 <jaindiya.k@gmail.com>
2025-10-31 16:52:47 +0530

    Update snake_gamebox.cpp
    opening the game screen on the another terminal screen

--- snake_gamebox.cpp
+++ snake_gamebox.cpp
@@ -11,91 +11,132 @@ struct Point {
     int y;
 };
 
+
 void gotoXY(int x, int y) {
     COORD coord = { (SHORT)x, (SHORT)y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+
+void clearScreen() {
+    system("cls");
+}
+
+
+void setupConsole() {
+    system("mode 80, 30");  
+    system("color 0A");     
+
 int main() {
+    setupConsole();
+
     const int BOX_HEIGHT = 20;
     const int BOX_WIDTH = 50;
 
-    vector<Point> snake;
-    snake.push_back({10, 10});
-    int snakeLength = 3;
-    int score = 0;
+    bool playAgain = true;
 
-    srand(time(0));
-    Point food = {rand() % (BOX_WIDTH - 2) + 1, rand() % (BOX_HEIGHT - 2) + 1};
+    while (playAgain) {
+        vector<Point> snake;
+        snake.push_back({10, 10});
+        int snakeLength = 3;
+        int score = 0;
 
-    char dir = 'd';
-    bool gameOver = false;
+        srand(time(0));
+        Point food = {rand() % (BOX_WIDTH - 2) + 1, rand() % (BOX_HEIGHT - 2) + 1};
 
-    while (!gameOver) {
-        gotoXY(0, 0);
-        cout << "Score: " << score << "\n\n";
+        char dir = 'd';
+        bool gameOver = false;
 
-        for (int row = 0; row < BOX_HEIGHT; ++row) {
-            for (int col = 0; col < BOX_WIDTH; ++col) {
-                if (row == 0 || row == BOX_HEIGHT - 1 || col == 0 || col == BOX_WIDTH - 1)
-                    cout << '#';
-                else {
-                    bool printed = false;
-                    if (row == food.y && col == food.x) {
-                        cout << 'F';
-                        printed = true;
-                    }
-                    for (auto s : snake) {
-                        if (row == s.y && col == s.x) {
-                            cout << 'O';
+        while (!gameOver) {
+            clearScreen();
+            cout << "Score: " << score << "\n\n";
+
+            
+            for (int row = 0; row < BOX_HEIGHT; ++row) {
+                for (int col = 0; col < BOX_WIDTH; ++col) {
+                    if (row == 0 || row == BOX_HEIGHT - 1 || col == 0 || col == BOX_WIDTH - 1)
+                        cout << '#';
+                    else {
+                        bool printed = false;
+                        if (row == food.y && col == food.x) {
+                            cout << 'F';
                             printed = true;
-                            break;
                         }
+                        for (auto s : snake) {
+                            if (row == s.y && col == s.x) {
+                                cout << 'O';
+                                printed = true;
+                                break;
+                            }
+                        }
+                        if (!printed) cout << ' ';
                     }
-                    if (!printed) cout << ' ';
                 }
+                cout << '\n';
             }
-            cout << '\n';
-        }
 
-        if (_kbhit()) {
-            char key = _getch();
-            if (key == 'w' && dir != 's') dir = 'w';
-            else if (key == 's' && dir != 'w') dir = 's';
-            else if (key == 'a' && dir != 'd') dir = 'a';
-            else if (key == 'd' && dir != 'a') dir = 'd';
-        }
+            
+            if (_kbhit()) {
+                char key = _getch();
+                if (key == 'w' && dir != 's') dir = 'w';
+                else if (key == 's' && dir != 'w') dir = 's';
+                else if (key == 'a' && dir != 'd') dir = 'a';
+                else if (key == 'd' && dir != 'a') dir = 'd';
+            }
 
-        Point newHead = snake[0];
-        if (dir == 'w') newHead.y--;
-        else if (dir == 's') newHead.y++;
-        else if (dir == 'a') newHead.x--;
-        else if (dir == 'd') newHead.x++;
+            
+            Point newHead = snake[0];
+            if (dir == 'w') newHead.y--;
+            else if (dir == 's') newHead.y++;
+            else if (dir == 'a') newHead.x--;
+            else if (dir == 'd') newHead.x++;
 
-        if (newHead.x <= 0 || newHead.x >= BOX_WIDTH - 1 || newHead.y <= 0 || newHead.y >= BOX_HEIGHT - 1)
-            gameOver = true;
-        for (auto s : snake) {
-            if (newHead.x == s.x && newHead.y == s.y)
+           
+            if (newHead.x <= 0 || newHead.x >= BOX_WIDTH - 1 || newHead.y <= 0 || newHead.y >= BOX_HEIGHT - 1)
                 gameOver = true;
+
+            
+            for (auto s : snake) {
+                if (newHead.x == s.x && newHead.y == s.y)
+                    gameOver = true;
+            }
+
+           
+            snake.insert(snake.begin(), newHead);
+            if (newHead.x == food.x && newHead.y == food.y) {
+                score++;
+                food.x = rand() % (BOX_WIDTH - 2) + 1;
+                food.y = rand() % (BOX_HEIGHT - 2) + 1;
+                snakeLength++;
+            } else {
+                while (snake.size() > snakeLength)
+                    snake.pop_back();
+            }
+
+            Sleep(120);
         }
 
-        snake.insert(snake.begin(), newHead);
-        if (newHead.x == food.x && newHead.y == food.y) {
-            score++;
-            food.x = rand() % (BOX_WIDTH - 2) + 1;
-            food.y = rand() % (BOX_HEIGHT - 2) + 1;
-            snakeLength++;
-        } else {
-            while (snake.size() > snakeLength) snake.pop_back();
-        }
+        
+        clearScreen();
+        cout << "\n\n\t===== GAME OVER =====\n";
+        cout << "\tFinal Score: " << score << "\n\n";
+        cout << "\tPress [R] to Restart or [Q] to Quit\n";
 
-        Sleep(150);
+        char choice;
+        while (true) {
+            choice = _getch();
+            if (choice == 'r' || choice == 'R') {
+                playAgain = true;
+                break;
+            } else if (choice == 'q' || choice == 'Q') {
+                playAgain = false;
+                break;
+            }
+        }
     }
 
-    gotoXY(0, BOX_HEIGHT + 2);
-    cout << "\n\n\tGame Over!\n";
-    cout << "\tFinal Score: " << score << "\n\n";
-
+    clearScreen();
+    cout << "\n\n\tThank you for playing Snake Game!\n";
+    
     return 0;
 }
-
